359c9e2b4bc8d417b90e23cafaf66425af5013b6
Jonester <chris.jones78@gmail.com>
2017-01-18 14:38:32 -0500

    Initial Commit

---
+++ HouseRooms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HouseRooms
+//
+//  Created by Chris Jones on 2017-01-18.
+//  Copyright © 2017 Jonescr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
